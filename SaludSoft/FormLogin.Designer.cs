d1e0b5dfda27b1631c2a7c0ad40bd06d219a1f87
Rojas Denise <karendeniserojas23@gmail.com>
2025-09-15 00:36:32 -0300

    Plantilla de admin en proceso, pantalla de inicio completa.

---
+++ SaludSoft/FormLogin.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SaludSoft
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lOlvidasteContraseña = new System.Windows.Forms.Label();
+            this.btIniciarSesion = new System.Windows.Forms.Button();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lOlvidasteContraseña);
+            this.panel1.Controls.Add(this.btIniciarSesion);
+            this.panel1.Controls.Add(this.tbContraseña);
+            this.panel1.Controls.Add(this.tbUsuario);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(878, 471);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lOlvidasteContraseña
+            // 
+            this.lOlvidasteContraseña.AutoSize = true;
+            this.lOlvidasteContraseña.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lOlvidasteContraseña.ForeColor = System.Drawing.Color.Red;
+            this.lOlvidasteContraseña.Location = new System.Drawing.Point(334, 406);
+            this.lOlvidasteContraseña.Name = "lOlvidasteContraseña";
+            this.lOlvidasteContraseña.Size = new System.Drawing.Size(226, 24);
+            this.lOlvidasteContraseña.TabIndex = 6;
+            this.lOlvidasteContraseña.Text = "¿Olvidaste tu contraseña?";
+            this.lOlvidasteContraseña.Click += new System.EventHandler(this.lOlvidasteContraseña_Click);
+            // 
+            // btIniciarSesion
+            // 
+            this.btIniciarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btIniciarSesion.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btIniciarSesion.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btIniciarSesion.Location = new System.Drawing.Point(338, 334);
+            this.btIniciarSesion.Name = "btIniciarSesion";
+            this.btIniciarSesion.Size = new System.Drawing.Size(209, 52);
+            this.btIniciarSesion.TabIndex = 4;
+            this.btIniciarSesion.Text = "Iniciar Sesión";
+            this.btIniciarSesion.UseVisualStyleBackColor = false;
+            this.btIniciarSesion.Click += new System.EventHandler(this.btIniciarSesion_Click);
+            // 
+            // tbContraseña
+            // 
+            this.tbContraseña.Location = new System.Drawing.Point(443, 272);
+            this.tbContraseña.Name = "tbContraseña";
+            this.tbContraseña.Size = new System.Drawing.Size(147, 26);
+            this.tbContraseña.TabIndex = 5;
+            this.tbContraseña.UseSystemPasswordChar = true;
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(443, 206);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(147, 26);
+            this.tbUsuario.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label4.Location = new System.Drawing.Point(253, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(388, 40);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Sistema de Gestión Médica";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(292, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(267, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 36F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(255, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(391, 102);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "SaludSoft";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(138, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(878, 464);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "\"SaludSoft - Login\"";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btIniciarSesion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbContraseña;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.Label lOlvidasteContraseña;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ SaludSoft/FormLogin.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaludSoft
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+            // Bloquear teclas no alfanuméricas
+            tbUsuario.KeyPress += SoloAlfanumerico_KeyPress;
+            tbContraseña.KeyPress += SoloAlfanumerico_KeyPress;
+
+            // Si pegan texto con caracteres inválidos, lo limpio
+            tbUsuario.TextChanged += SanitizarAlPegar;
+            tbContraseña.TextChanged += SanitizarAlPegar;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btIniciarSesion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lOlvidasteContraseña_Click(object sender, EventArgs e)
+        {
+        MessageBox.Show(
+        "Contactá al administrador o usa el proceso de recuperación.",
+        "Olvidé mi contraseña",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Information
+    );
+        }
+
+        // 1) Bloquea en el momento de tipear (no permite escribir nada que no sea letra o número)
+        private void SoloAlfanumerico_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir teclas de control (Backspace, Delete, flechas, Tab, Enter, etc.)
+            if (char.IsControl(e.KeyChar))
+                return;
+
+            // Bloquear espacios
+            if (e.KeyChar == ' ')
+            {
+                e.Handled = true;
+                return;
+            }
+
+            // Permitir sólo letras y números (incluye letras acentuadas)
+            if (!char.IsLetterOrDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        // 2) Si pegan texto con Ctrl+V u otro método, se limpia lo que no sea alfanumérico
+        private void SanitizarAlPegar(object sender, EventArgs e)
+        {
+            var tb = (TextBox)sender;
+            int caret = tb.SelectionStart;
+
+            var limpio = new System.Text.StringBuilder(tb.Text.Length);
+            foreach (char c in tb.Text)
+                if (char.IsLetterOrDigit(c)) limpio.Append(c);
+
+            if (tb.Text != limpio.ToString())
+            {
+                tb.Text = limpio.ToString();
+                // Restauro la posición del cursor lo mejor posible
+                tb.SelectionStart = Math.Min(caret, tb.Text.Length);
+            }
+        }
+
+    }
+}
