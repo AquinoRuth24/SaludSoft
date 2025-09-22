1a4a15a718a8789fbf806b89fbd23c6f46431393
Denise <Usuario@DESKTOP-JON2R33>
2025-09-22 15:54:17 -0300

    Pantalla inicio en FormLogin

--- SaludSoft/Program.cs
+++ SaludSoft/Program.cs
@@ -18,10 +18,10 @@ namespace SaludSoft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormLogin());
+            Application.Run(new FormLogin());
             // Application.Run(new SaludSoft());
            // Application.Run(new Admin());
-            Application.Run(new Medico());
+            //Application.Run(new Medico());
             //Application.Run(new FormGestionUsuario());
 
         }
